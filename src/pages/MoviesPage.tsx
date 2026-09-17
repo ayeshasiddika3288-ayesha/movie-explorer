7ffd79fee04b2b12b51abe1220b75631c28619df
Ayesha Siddika <ayeshasiddika3288@gmail.com>
2026-09-17 13:21:29 +0600

    Add animated loading spinner

--- src/pages/MoviesPage.tsx
+++ src/pages/MoviesPage.tsx
@@ -56,10 +56,12 @@ function MoviesPage() {
         </div>
       </div>
 
-      {/* States: loading, error, empty, results */}
       {loading && (
-        <p className="text-center text-gray-500 py-10">Loading movies...</p>
-      )}
+  <div className="flex flex-col items-center justify-center py-20">
+    <div className="w-10 h-10 border-4 border-blue-600 border-t-transparent rounded-full animate-spin mb-4"></div>
+    <p className="text-gray-500">Loading movies...</p>
+  </div>
+)}
 
       {!loading && error && (
         <p className="text-center text-red-500 py-10">{error}</p>
